622374b7cae5be425183262bb8e4209d304d4b94
Joschi Kuphal <joschi@kuphal.net>
2016-02-23 22:36:08 +0100

    Added Rodney Rehm / ally.js

---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="de" xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<?php echo file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'css'.DIRECTORY_SEPARATOR.'icons-loader-fragment.html'); ?>
+
+		<meta charset="utf-8" />
+		<title>Accessibility Club #3 — Treffen für Webworker zu Barrierefreiheit &amp; assistiven Technologien — 12. April 2016, Nürnberg Web Week 2016</title>
+		<meta name="author" content="tollwerk® GmbH | Dipl.-Ing. Joschi Kuphal" />
+		<meta name="robots" content="index,follow" />
+		<meta name="mobileOptimized" content="width" />
+		<meta name="handheldFriendly" content="true" />
+		<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+		<meta name="format-detection" content="telephone=no" />
+		<meta name="keywords" content="Accessibility, Barrierefreiheit, Assistive Technologie, Screenreader, Open Device Lab, tollwerk, Joschi Kuphal, Nürnberg, Nuernberg" />
+		<meta name="description" content="Accessibility Club #3 — Treffen für Webworker zu Barrierefreiheit &amp; assistiven Technologien — 12. April 2016, Nürnberg Web Week 2016" />
+
+		<meta name="msapplication-square70x70logo" content="/favicons/windows-tile-70x70.png"/>
+		<meta name="msapplication-square150x150logo" content="/favicons/windows-tile-150x150.png"/>
+		<meta name="msapplication-square310x310logo" content="/favicons/windows-tile-310x310.png"/>
+		<meta name="msapplication-TileImage" content="/favicons/windows-tile-144x144.png"/>
+		<meta name="msapplication-TileColor" content="#323232"/>
+		<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/favicons/apple-touch-icon-152x152-precomposed.png"/>
+		<link rel="apple-touch-icon-precomposed" sizes="120x120" href="/favicons/apple-touch-icon-120x120-precomposed.png"/>
+		<link rel="apple-touch-icon-precomposed" sizes="76x76" href="/favicons/apple-touch-icon-76x76-precomposed.png"/>
+		<link rel="apple-touch-icon-precomposed" sizes="60x60" href="/favicons/apple-touch-icon-60x60-precomposed.png"/>
+		<link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-touch-icon-144x144.png"/>
+		<link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-touch-icon-114x114.png"/>
+		<link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-touch-icon-72x72.png"/>
+		<link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-touch-icon.png"/>
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+		<link rel="icon" href="/favicons/favicon.png" type="image/png" sizes="64x64" />
+		<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,700" media="all" />
+		<link rel="stylesheet" type="text/css" href="css/a11y-above.min.css" media="all" />
+
+		<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script><![endif]-->
+		<!--[if gte IE 9]><style type="text/css">.gradient{filter: none}</style><![endif]-->
+
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Accessibility Club #3 — 12. April 2016, Nürnberg Web Week 2016" />
+		<meta property="og:description" content="Treffen für Webentwickler &amp; -designer zur Förderung von Barrierefreiheit im Internet und zum Austausch über assistive Technologien — 12. April 2016, Nürnberg Web Week 2016" />
+		<meta property="og:image" content="http://accessibility-club.org/a11y.png" />
+		<meta property="og:url" content="http://accessibility-club.org" />
+		<meta property="og:site_name" content="Accessibility Club #3 — 12. April 2016, Nürnberg Web Week 2016" />
+
+		<meta name="twitter:card" content="summary">
+		<meta name="twitter:site" content="@tollwerk">
+		<meta name="twitter:creator" content="@tollwerk">
+		<meta name="twitter:image" content="http://accessibility-club.org/a11y.png">
+		<meta name="twitter:description" content="Treffen für Webentwickler & -designer zur Förderung von Barrierefreiheit im Internet und zum Austausch über assistive Technologien — 12. April 2016, Nürnberg Web Week 2016">
+		<meta name="twitter:title" content="Accessibility Club #3 — 12. April 2016, Nürnberg Web Week 2016">
+		<meta name="twitter:url" content="http://accessibility-club.org">
+
+        <link rel=canonical href=“http://accessibility-club.org” />
+	</head>
+	<body>
+		<header role="banner" class="h-event gradient" itemscope="itemscope" itemtype="http://data-vocabulary.org/Event">
+            <div class="nueww icons-nueww2016 icons-nueww2016-dims">Eine Veranstaltung im Rahmen der Nürnberg Web Week 2016</div>
+            <div class="header">
+                <a href="http://accessibility-club.org" class="u-url" itemprop="url"><img itemprop="photo" alt="Accessibility Club #3 — 12. April 2016, Nürnberg Web Week 2016" src="/img/accessibility-club-logo.png" width="255" height="255"/></a>
+                <h1 class="p-name">Accessibility Club</h1>
+                <span class="icons-a11y icons-a11y-dims p-summary" itemprop="summary">Treffen für Webentwickler &amp; -designer zur Förderung von Barrierefreiheit im Internet und zum Austausch über assistive Technologien</span>
+                <div class="dateblock">
+                    <p class="label scheduled">in der Nürnberg Web Week am</p>
+                    <time class="dt-start" itemprop="dtstart" datetime="2016-04-12T16:00+02:00">12. April 2016</time>
+                    <time class="dt-end hide" itemprop="dtend" datetime="2016-04-12T22:00+02:00"></time>
+                    <p class="label at">16:00 bis 22:00 Uhr im</p>
+                </div>
+                <a class="address" href="https://www.google.de/maps/place/tollwerk+GmbH+%7C+TYPO3-+%26+Werbeagentur+N%C3%BCrnberg/@49.4751594,11.1067807,15z/data=!4m2!3m1!1s0x0:0x84a1731ed749b69f?sa=X&ved=0CIABEPwSMBJqFQoTCO-s0vS12cgCFcRIFAod4hoKDg" target="_blank">
+                    <address class="p-location h-card icons-location" itemprop="location" itemscope="itemscope" itemtype="http://data-vocabulary.org/Organization">
+                        <div class="p-name p-label p-org" itemprop="name">tollwerk</div>
+                        <div class="p-adr h-adr" itemprop="address" itemscope="itemscope" itemtype="http://data-vocabulary.org/Address">
+                            <div class="p-street-address" itemprop="street-address">Klingenhofstraße 5</div>
+                            <div>
+                                <span class="p-postal-code" itemprop="postal-code">90411</span>
+                                <span class="p-locality" itemprop="locality">Nürnberg</span>
+                                <span class="hide">, <span class="p-country" itemprop="country-name">Deutschland</span></span>
+                            <span class="hide" itemprop="geo" itemscope="itemscope" itemtype="http://data-vocabulary.org/Geo">
+                                <span class="p-latitude" itemprop="latitude">49.4751594</span>
+                                <span class="p-longitude" itemprop="longitude">11.1067807</span>
+                            </span>
+                            </div>
+                            <div class="p-region hide" itemprop="region">Bayern</div>
+                        </div>
+                    </address>
+                </a>
+            </div>
+		</header>
+		<main role="main">
+			<section>
+				<div class="left content">
+					<h2>Barrierefreiheit und assistive Technologien</h2>
+					<p>Obwohl die Notwendigkeit und der Mehrwert <a href="http://de.wikipedia.org/wiki/Barrierefreies_Internet" target="_blank">barrierefreier</a> Angebote im Internet auf der Hand liegen, finden etablierte Standards zur Umsetzung bisher kaum allgemeine Anwendung. Ein vermeintlich zwangsläufiger Mehraufwand, knappe Budgets und der gängige Irrtum <q>Validität = Barrierefreiheit</q> — die auf Nachfrage angeführten Gründe sind vielfältig. In vielen Fällen ist jedoch schlicht die Ahnungslosigkeit der beteiligten Webentwickler und -designer die Ursache für unnötige Barrieren. Kein Wunder, sind doch bislang nur die Wenigsten selbst mit <a href="http://de.wikipedia.org/wiki/Unterst%C3%BCtzungstechnologie" target="_blank">assistiven Technologien</a> auf Tuchfühlung gegangen.</p>
+					<p>Mit dem Accessibility Club möchten wir Dir eine handfeste Möglichkeit bieten, Erfahrungen mit Barrierefreiheit im Web zu sammeln, zu vertiefen und mit anderen Interessierten auszutauschen. Ziel soll es sein, dass Du barrierefreie Techniken standardmäßig in deine eigene Arbeitswelt übernehmen und zur Selbstverständlichkeit werden lassen kannst.</p>
+				</div>
+				<div class="right content">
+					<h2>Accessibility-Treffen für Webworker</h2>
+					<p>Aufbauend auf den beiden vorangegangenen Malen soll vor allem der <b>praktische Einsatz barrierefreier Techniken</b> im Mittelpunkt des dritten Accessibility Clubs stehen, es ist also kollektive Code-Nerdery zu erwarten. Das Treffen ist natürlich wieder <b>vollkommen kostenlos</b> und reiht sich damit in die lange Liste spannender Veranstaltungen der <a href="http://nueww.de" target="_blank">Nürnberg Web Week 2016</a> ein — Dabei-Sein lohnt sich in jedem Fall!</p>
+					<p>Auch beim dritten Mal wird <a href="https://jkphl.is" target="_blank">Joschi Kuphal</a> am Veranstaltungsabend wieder von <b>Willi Lutzenberger</b> und seinem Team tatkräftig unterstützt: Als Spezialist für assistive Technologien — und selbst blind — bietet er mit seinem Nürnberger Unternehmen <a href="http://www.protak.net">Protak</a> EDV-Lösungen und Dienstleistungen, um Blinde und Sehbehinderte sowohl im beruflichen als auch privaten Alltag bei der Erlangung maximaler Eigenständigkeit, Unabhängigkeit und Inklusion zu unterstützen.</p>
+					<p><b>Hast Du zufällig Lust und Energie</b>, den Accessibility Club organisatorisch zu unterstützen? Oder ist eher die Geselligkeit Dein Ding und Du möchtest eine Runde Pizza für alle spendieren? Wir freuen uns natürlich über <a href="mailto:hello@a11y-club.org?subject=Unterst%C3%BCtzung%20f%C3%BCr%20den%20Accessibility%20Club" title="Ja, ich möchte den Accessibility Club unterstützen!">jede Form von Unterstützung!</a></p>
+				</div>
+			</section>
+			<section>
+				<div class="left content">
+					<h2>JAWS-Demonstration vom Spezialisten</h2>
+					<p>Beim Accessibility Club soll es vor allem um das gemeinsame Sammeln und Austauschen von Erfahrungen gehen, weniger um Frontalvorträge.</p>
+					<p>Im ersten Teil <b>von 16 bis 17 Uhr</b> wird Willi Lutzenberger mit seinem Team einen Einblick in die Funktion und Bedienung von <a href="http://de.wikipedia.org/wiki/Screenreader" target="_blank">Screenreadern</a> am Beispiel von <a href="http://www.freedomsci.de/prod01.htm" target="_blank">JAWS</a> geben. Wenn Du möchtest, kannst Du eines Deiner Projekte mit einem Screenreader erforschen und Fragen dazu zu klären.</p>
+					<p>Nach einer kurzen Pause wollen wir die Abendstunden mit der <b>praktischen Anwendung barrierefreier Techniken</b> oder Gesprächsrunden über verschiedenste Themen verbringen. Je nach Teilnehmerzahl bietet sich eine <a href="https://de.wikipedia.org/wiki/Tagung#Unkonferenz" target="_blank">unkonferenzartige</a> Vorab-Sessionplanung an.</p>
+				</div>
+				<div class="right content">
+					<h2>ally.js — JavaScript-Helfer für Websites</h2>
+					<p>Als weit angereisten Gast dürfen wir uns dieses Mal besonders auf <a href="http://rodneyrehm.de" target="_blank">Rodney Rehm</a> freuen, der uns <b>ab 17 Uhr</b> seine noch junge JavaScript-Bibliothek <a href="http://allyjs.io" target="_blank">ally.js</a> vorstellen und demonstrieren wird.</p>
+                    <p class="portrait">
+                        <img src="/img/rodney-rehm-a11y.jpg" alt="Portrait von Rodney Rehm"/>
+                    </p>
+					<p><strong>ally.js</strong> hilft Website-Entwicklern durch die Vereinfachung häufig genutzter Accessibility-Features und -Funktionen und die Egalisierung von Browserunterschieden.</p>
+				</div>
+		   	</section>
+
+		   	<section>
+		   		<div class="left content">
+                    <h2>Im Anschluss: Themen-Sessions</h2>
+                    <p>Mögliche Schwerpunkte wären etwa:</p>
+                    <ul>
+                        <li><a href="http://en.wikipedia.org/wiki/Semantic_HTML" target="_blank">Semantisches HTML</a>: Sinnvoller Einsatz, verkannte und unbekannte Elemente</li>
+                        <li>Nutzung von <a href="http://www.w3.org/WAI/intro/aria" target="_blank">WAI-ARIA</a> in der Praxis</li>
+                        <li>Barrierefreies <a href="https://de.wikipedia.org/wiki/Scalable_Vector_Graphics" target="_blank">SVG</a></li>
+                        <li>Tools und Hilfestellung für die barrierfreie Entwicklung</li>
+                        <li>Barrierefreiheits-Ertüchtigung für Content-Redakteure</li>
+                        <li>Barrierefreiheit in <a href="https://de.wikipedia.org/wiki/Content-Management-System" target="_blank">Content-Management-Systemen</a></li>
+                        <li>Argumentationshilfen für Verkaufs- und Konzeptionsgespräche</li>
+                        <li>usw.</li>
+                    </ul>
+                    <p>Zu manchen dieser Themen können wir womöglich selbst etwas erzählen, vor allem aber sind wir auf <b>Deinen Beitrag</b> gespannt! Bring gerne am Veranstaltungstag eigene Vorschläge mit oder lass uns <a href="mailto:hello@a11y-club.org?subject=Themenvorschlag" title="Themenvorschlag">im Vorfeld wissen</a>, was Dich noch interessiert. Wir freuen uns!</p>
+		   		</div>
+		   		<div class="right content" id="proposal">
+                    <h2>Anmeldung & Treffpunkt</h2>
+                    <p>Der nächste Accessibility Club trifft sich <b>am 12. April 2016</b> um <b>16 Uhr</b> in den neuen <b>tollwerk Veranstaltungsräumen</b>. Die Teilnahme ist kostenlos.<p>
+                    <blockquote>WICHTIG: Wir bitten um Anmeldung per E-Mail an <a href="mailto:hello@a11y-club.org?subject=Teilnahme">hello@a11y-club.org</a></blockquote>
+                    <p>Alternativ kannst Du dich auch über <a href="https://www.facebook.com/events/1133509876661843/" target="_blank">Facebook</a> oder <a href="https://www.xing.com/events/accesibility-club-3-1614918" target="_blank">XING</a> anmelden. Nutze außerdem die <a href="http://lanyrd.com/cfwfqf" target="_blank">Lanyrd-Seite</a> zum Event, um Deinen Freunden zu zeigen, dass du teilnimmst.</p>
+		   			<h2>Und in Zukunft?</h2>
+		   			<p>Ab dem dritten Mal soll der Accessiblity Club im Bestfall regelmäßig stattfinden — hilf uns bei der Organisation und nimm <a href="mailto:hello@a11y-club.org">Kontakt zu uns</a> auf. Sollest Du Lust haben, etwas ähnliches zu veranstalten, vielleicht sogar in einer anderen Region, dann freuen wir uns auf den Austausch mit Dir!</p>
+		   		</div>
+		   	</section>
+		</main>
+		<footer>
+			<section>
+				<div class="left content">
+					<p class="by">Eine Initiative von:</p>
+					<address class="h-card icons-tollwerk" itemscope="itemscope" itemtype="http://data-vocabulary.org/Organization">
+						<div class="p-name p-label p-org" itemprop="name">tollwerk<sup>®</sup> GmbH</div>
+						<div class="p-adr h-adr" itemprop="address" itemscope="itemscope" itemtype="http://data-vocabulary.org/Address">
+							<div class="p-street-address" itemprop="street-address">Klingenhofstraße 5</div>
+							<div>
+								<span class="p-postal-code" itemprop="postal-code">90411</span>
+								<span class="p-locality" itemprop="locality">Nürnberg</span>
+								<span class="hide">, <span class="p-country" itemprop="country-name">Deutschland</span></span>
+								<span class="hide" itemprop="geo" itemscope="itemscope" itemtype="http://data-vocabulary.org/Geo">
+									<span class="p-latitude" itemprop="latitude">49.4751594</span>
+									<span class="p-longitude" itemprop="longitude">11.1067807</span>
+								</span>
+							</div>
+							<div class="p-region hide" itemprop="region">Bayern</div>
+						</div>
+						<dl>
+							<dt>Internet</dt>
+							<dd><a href="http://tollwerk.de" target="_blank" class="u-url" itemprop="url">http://tollwerk.de</a></dd>
+							<dt>E-Mail</dt>
+							<dd><a href="mailto:info@tollwerk.de" target="_blank" class="u-email">info@tollwerk.de</a></dd>
+						</dl>
+					</address>
+				</div>
+				<div class="right content">
+					<p class="by">Unterstützt durch:</p>
+					<address class="h-card icons-protak" itemscope="itemscope" itemtype="http://data-vocabulary.org/Organization">
+						<div class="p-name p-label p-org" itemprop="name">ProTak GmbH</div>
+						<div class="p-adr h-adr" itemprop="address" itemscope="itemscope" itemtype="http://data-vocabulary.org/Address">
+							<div class="p-street-address" itemprop="street-address">Königstraße 73</div>
+							<div>
+								<span class="p-postal-code" itemprop="postal-code">90402</span>
+								<span class="p-locality" itemprop="locality">Nürnberg</span>
+								<span class="hide">, <span class="p-country" itemprop="country-name">Deutschland</span></span>
+								<span class="hide" itemprop="geo" itemscope="itemscope" itemtype="http://data-vocabulary.org/Geo">
+									<span class="p-latitude" itemprop="latitude">49.451724</span>
+									<span class="p-longitude" itemprop="longitude">11.0775409</span>
+								</span>
+							</div>
+							<div class="p-region hide" itemprop="region">Bayern</div>
+						</div>
+						<dl>
+							<dt>Internet</dt>
+							<dd><a href="http://protak.net" target="_blank" class="u-url" itemprop="url">http://protak.net</a></dd>
+							<dt>E-Mail</dt>
+							<dd><a href="mailto:kontakt@protak.net" target="_blank" class="u-email">kontakt@protak.net</a></dd>
+						</dl>
+					</address>
+				</div>
+			</section>
+		</footer>
+	</body>
+</html>
